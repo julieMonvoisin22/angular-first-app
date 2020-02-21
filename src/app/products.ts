8a315656d0d2a400878b5f53822dff0933c73086
julieMonvoisin22 <juliemonvoisin@gmail.com>
2020-02-21 10:01:52 +0100

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,33 @@
 export const products = [
   {
-    productId:1,
     name: 'Phone XL',
     price: 799,
     description: "Un grand téléphone avec l'un des meilleurs écrans"
   },
   {
-    productId:2,
     name: 'Phone Mini',
     price: 699,
     description: "Un super téléphone avec l'un des meilleurs appareils photo"
   },
   {
-    productId:2,
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    name: 'Huawei P30',
+    price: 599,
+    description: "Un grand téléphone avec l'un des meilleurs écrans"
+  },
+  {
+    name: 'Samsung 10',
+    price: 1099,
+    description: "Un super téléphone avec l'un des meilleurs appareils photo"
+  },
+  {
+    name: 'nokia3210',
+    price: 299,
+    description: 'Il faut aimer les touches !'
   }
 ];
 
